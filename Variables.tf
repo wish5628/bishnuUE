c43204dd3203f5aeddaed25f631ed8ba4e71e8d6
wish5628 <106404102+wish5628@users.noreply.github.com>
2022-06-12 21:29:32 +0200

    Add files via upload

---
+++ Variables.tf
@@ -0,0 +1,11 @@
+provider "aws" {
+  region = "us-east-1"
+  access_key = "ASIAVRTHRKST35BMHUII"
+  secret_key = "O3lfXArsUnurI53t1ZXIT7mimxJ6TrqbgqwdUwB/"
+  token = "FwoGZXIvYXdzEOb//////////wEaDB8e75SpFhRMQINTUCLTAbZD0mla95ILpmA8Z7UU+0a3yX0AjHn7j9EhhHSem9R7PXS8ny0mnFW9mco01ntKX/7Z0eRq7j+rFqDg+FLoQf4KMBdVbdsFl7LPaZmUaK1j/EaETSFmVUFGJVvTc7XVQZ1LVa4xRtIJnzrxC+HlMk5MtBLXi8m1udJUluCKNv4mPYCbcyyT18n2xzZnKt5cag+847zN3MlBUnsFxu4lHb9zzz7Uf3J5+flpQRRa7FctWY+BR2UIfjF3DH/CRPiAfeJ+1+3TsceJXiVA51txqzObme4ovInDlAYyLYyvdxRi2zID9cLVN0FxkVuTSBrVJEVgnv9CALZsLLyAkMVDfS2A0UTlnnDsiw=="
+          
+             }
+
+
+
+
